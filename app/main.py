e2a09b89ef19b47e55e45f3e75f5a7a86afb7ceb
ochiai444 <ochanghui7@gmail.com>
2026-07-21 11:13:28 +0900

    프로젝트 초기 설정

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from database import engine
+from models import Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"message": "AI Study Planner"}
